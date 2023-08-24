39efbc68f379efd686228eb4a35240e8ecbf14e8
cr7st74n <cris-082011@hotmail.com>
2023-08-24 18:38:07 -0400

    Working in some changes, and adding more features.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # This is a sample Python script.
-
+import datetime
 
 print("Hello!  Can I ask your name and the month that you were born ?")
 name = input("Name: ")
+
 month = input("Month: ")
+month.capitalize()
+
+nameNumber = len(name.strip())
+
+currentMonth = datetime.datetime.now().strftime('%B')  # I found this way to get the current month in text
+
+# Conditional
+if month == currentMonth:
+    print(' "Happy Birthday month" :D !')
+else:
+    print("It's not your birthday yet :)")
 
 print(f'Welcome to class {name} and your birthday will be in {month}')
+
+print(f'The number of letters in your name is {nameNumber} ')
+
