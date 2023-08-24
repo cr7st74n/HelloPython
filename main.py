ae235f3e13d95b5a8e6c176fd21d6666c00997dd
cr7st74n <cris-082011@hotmail.com>
2023-08-23 21:07:12 -0400

    first changes

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+
+print("Hello!  Can I ask your name and the month that you were born ?")
+name = input("Name: ")
+month = input("Month: ")
+
+print(f'Welcome to class {name} and your birthday will be in {month}')
