47052d5dfc6dacde747695c22164447260461e11
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-09-08 09:39:24 -0400

    adding a camelCase Funtion

--- camelCase.py
+++ camelCase.py
@@ -1,19 +1,23 @@
 # Camel Case
 
-originalSentence = input("Enter your sentence: ")
+def camelCase():
+    originalSentence = input("Enter your sentence: ")
+    if originalSentence.isalnum() or originalSentence.isalnum():
+        print("this is not a string character or a sentence, try again ! please :,(")
+    else:
+        newSentence = originalSentence.split()
 
-if originalSentence.isalnum() or originalSentence.isalnum():
-    print("this is not a string character or a sentence, try again ! please :,(")
-else:
-    newSentence = originalSentence.split()
+        # use a for loop to check the sentence and modify to a lower case, and capitalize.
+        for i in range(0, len(newSentence)):
+            newSentence[i] = newSentence[i].lower()
+            newSentence[i] = newSentence[i].capitalize()
 
-    # use a for loop to check the sentence and modify to a lower case, and capitalize.
-    for i in range(0, len(newSentence)):
-        newSentence[i] = newSentence[i].lower()
-        newSentence[i] = newSentence[i].capitalize()
+        # here we join all the words and to print, we lower the first character.
+        output = "".join(newSentence)
 
-    # here we join all the words and to print, we lower the first character.
-    output = "".join(newSentence)
+        sentenceCaCase = print(f' - {output[0].lower() + output[1:]} ')
 
-    print(f' - {output[0].lower() + output[1:]} ')
+        return sentenceCaCase
+    
+camelCase()
 
